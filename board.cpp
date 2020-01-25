38c198d0ff8043c1be236f4c431545a400625c9e
noname1477 <54987242+noname1477@users.noreply.github.com>
2020-01-25 14:01:12 +0100

    Made print_board() actually work

--- board.cpp
+++ board.cpp
@@ -44,9 +44,9 @@ void Board::throw_invalid_cell_exception(int x, int y) {
 }
 
 void Board::print_board() {
-	std::cout << " " << board[0][0].get_status() << "|" << board[1][0].get_status() << "|" << board[2][0].get_status() << std::endl;
-	std::cout << "—+-+—" << std::endl;
-	std::cout << " " << board[0][0].get_status() << "|" << board[1][0].get_status() << "|" << board[2][0].get_status() << std::endl;
-	std::cout << "—+-+—" << std::endl;
-	std::cout << " " << board[0][0].get_status() << "|" << board[1][0].get_status() << "|" << board[2][0].get_status() << std::endl;
+	std::cout << " " << board[0][0].get_status() << " | " << board[1][0].get_status() << " | " << board[2][0].get_status() << std::endl;
+	std::cout << "---+---+---" << std::endl;
+	std::cout << " " << board[0][1].get_status() << " | " << board[1][1].get_status() << " | " << board[2][1].get_status() << std::endl;
+	std::cout << "---+---+---" << std::endl;
+	std::cout << " " << board[0][2].get_status() << " | " << board[1][2].get_status() << " | " << board[2][2].get_status() << std::endl;
 }
